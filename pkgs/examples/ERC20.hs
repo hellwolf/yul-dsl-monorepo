ad2b9a93eb415a60afa814900002bd9fd5f9a77d
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-07-05 16:42:12 +0300

    split ERC20 example module out

---
+++ pkgs/examples/ERC20.hs
@@ -0,0 +1,23 @@
+module ERC20 where
+
+import           LoliYul.Core
+
+-- | ERC20 balance storage location for the account.
+-- TODO should use hashing of course.
+erc20_balance_storage :: forall r. YulObj r => AddrP r ⊸ AddrP r
+erc20_balance_storage account = mkUnit account & \(account, unit) -> yulCoerce $
+  yulCoerce account + yulConst (to_intx @UINT256 0x42) unit
+
+-- | ERC20 balance of the account.
+erc20_balance_of :: YulObj r => AddrP r ⊸ Uint256P r
+erc20_balance_of account = sget (erc20_balance_storage account)
+
+-- | ERC20 transfer function (no negative balance check for simplicity).
+erc20_transfer :: Fn (ADDR :> ADDR :> UINT256 :> ()) (One BOOL)
+erc20_transfer = defun "transfer" \(from :> to :> amount :> ()) ->
+  (copyAp amount
+    (\amount -> passAp from erc20_balance_of & \(from, balance) ->
+        erc20_balance_storage from <== balance - amount)
+    (\amount -> passAp to erc20_balance_of & \(to, balance) ->
+        erc20_balance_storage to <== balance + amount)) &
+  yulConst (true :> ())
